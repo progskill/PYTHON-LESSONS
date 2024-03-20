af3ea64f50876e35e963063b1ce67a0ae31a6abf
Evans-mutuku <evansnyamai98@gmail.com>
2024-03-20 19:51:50 +0300

    Python OOP

--- OOP/myoop.py
+++ OOP/myoop.py
@@ -9,6 +9,8 @@ class Vehicle:
 
     def stop(self):
         raise NotImplementedError("Subclass must implement this abstract method")
+
+
 class Car(Vehicle):
     def start(self):
         return f"The {self.year} {self.make} {self.model} starts"
@@ -16,29 +18,14 @@ class Car(Vehicle):
     def stop(self):
         return f"The {self.year} {self.make} {self.model} stops"
 
-class Motorcycle(Vehicle):
-    def start(self):
-        return f"The {self.year} {self.make} {self.model} revs the engine and starts"
 
-    def stop(self):
-        return f"The {self.year} {self.make} {self.model} applies the brakes and stops"
-
-class Bike(Vehicle):
-    def start(self):
-        return f"The {self.year} {self.make} {self.model} revs the engine and starts"
-    
-    def stop(self):
-        return f"The {self.year} {self.make} {self.model} applies the brakes and stops"
-    
 # Creating instances of Car and Motorcycle
 my_car = Car("Toyota", "Camry", 2020)
-my_motorcycle = Motorcycle("Honda", "CBR500R", 2019)
 
 # Using the start and stop methods without needing to know the internal implementation
 print(my_car.start())
 print(my_car.stop())
-# print(my_motorcycle.start())
-# print(my_motorcycle.stop())
+
 
 
 
