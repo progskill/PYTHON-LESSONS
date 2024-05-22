94009595648117113c302110f5f30ef3f8a27092
Evans-mutuku <evansnyamai98@gmail.com>
2024-05-22 10:57:28 +0300

    Hello world

--- DJANGO PROJECTS/poll/mysite/myapp/views.py
+++ DJANGO PROJECTS/poll/mysite/myapp/views.py
@@ -40,7 +40,6 @@ def vote(request, question_id):
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 # def vote(request, question_id):
 #     return HttpResponse("You're voting on question %s." % question_id)
