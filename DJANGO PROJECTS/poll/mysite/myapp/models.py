9a4c471692cc76a570960471fbea15f09e511336
Evans-mutuku <evansnyamai98@gmail.com>
2024-05-22 12:59:27 +0300

    Hello world

--- DJANGO PROJECTS/poll/mysite/myapp/models.py
+++ DJANGO PROJECTS/poll/mysite/myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+
 
 import datetime
 from django.utils import timezone
@@ -9,6 +11,11 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
