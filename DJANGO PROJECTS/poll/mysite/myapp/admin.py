ce293d797c3781fe4cb8631e5d490950e8fa6c49
Evans-mutuku <evansnyamai98@gmail.com>
2024-05-22 12:44:50 +0300

    Hello world

--- DJANGO PROJECTS/poll/mysite/myapp/admin.py
+++ DJANGO PROJECTS/poll/mysite/myapp/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Question
 
 # Register your models here.
-
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    
 admin.site.register(Question)
